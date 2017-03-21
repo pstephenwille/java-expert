5f178a0efb4ed00d7ab415fb93000b7bb7c9c515
stephen.wille <stephen.wille@gmail.com>
2017-03-20 19:53:52 -0700

    INIT:slack api

--- src/main/java/com/swille/JavaExpertApplication.java
+++ src/main/java/com/swille/JavaExpertApplication.java
@@ -1,16 +1,19 @@
 package com.swille;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.swille")
+@Configuration
+@EnableAutoConfiguration
 public class JavaExpertApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaExpertApplication.class, args);
-		People steve = new  People();
-		steve.setName("steve");
-		System.out.println(steve.getName());
 	}
 
 
